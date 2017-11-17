5f0b52ad42ac8b4e6bc13c4474ab288b2a8f6cac
Santosh Singh <singh.santhosh84@gmail.com>
2017-11-17 10:45:55 +0530

    Added Line Strip to Insert Database Function

--- generate_bt_utils.py
+++ generate_bt_utils.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 import MySQLdb
-from re import compile
+import re 
 from prettytable import PrettyTable as pp
 import jira_utils
 from datetime import datetime
@@ -10,7 +10,7 @@ from datetime import datetime
 def get_contents(stack_trace):
     contents = ""
     multi_thread = False
-    thread_pattern = compile('Thread\s[0-9]{1}')
+    thread_pattern = re.compile('Thread\s[0-9]{1,2}')
 
     with open(stack_trace, 'r') as f:
         for line in f:
@@ -22,14 +22,14 @@ def get_contents(stack_trace):
     return (contents, multi_thread)  
 
 def get_function_list_signal_from_multi_thread_stack_trace(contents):
-    func_pattern = compile('\s(\S*)\s\(')
+    func_pattern = re.compile('\s(\S*)\s\(')
     signal = ''
     func_name = []
     crash_func = ''
     match = False
     thread_group = []
 
-    contents_group = [line for line in contents.split("\n\n")]
+    contents_group = [line for line in re.split('Thread\s[0-9]+\s', contents)]
     
 
     for non_thread in contents_group[0].split("\n"):
@@ -40,9 +40,10 @@ def get_function_list_signal_from_multi_thread_stack_trace(contents):
                 crash_func = func_pattern.search(non_thread).group(1)
 
     for temp in contents_group[1:]:
-        thread_group.append(temp.split("\n"))
+        if crash_func in temp:
+            thread_group.append(temp.split("\n"))
 
-    for thread_list in thread_group:
+    for thread_list in thread_group[:1]:
         for line in thread_list:
             if (line.startswith('#') and '??' not in line) :
                 if func_pattern.search(line):
@@ -54,7 +55,7 @@ def get_function_list_signal_from_multi_thread_stack_trace(contents):
     return (func_name, signal)                                                                                                                                                                                                                             
 
 def get_function_list_signal_from_stack_trace(stack_trace):
-    func_pattern = compile('\s(\S*)\s\(')
+    func_pattern = re.compile('\s(\S*)\s\(')
     signal = ''
     func_list = []
     
@@ -78,11 +79,15 @@ def get_function_list_signal_from_stack_trace(stack_trace):
 
 def mysql_connect():
     
-    conn = MySQLdb.connect(host='10.10.25.137', \
-                    port = 3306,\
-                    db='jenkins_report_portal',\
-                    user='root',\
-                    passwd='avi123mysql')
+    try:
+        conn = MySQLdb.connect(host='10.10.25.137', \
+                        port = 3306,\
+                        db='jenkins_report_portal',\
+                        user='root',\
+                        passwd='avi123mysql')
+    except MySQLdb.Error as e:
+        print "MySql Operation Failed because %s" %(str(e))
+        exit(0)
 
     return conn
 
@@ -90,6 +95,7 @@ def mysql_connect():
 
 def check_for_duplicates_in_db(func_list, signal):
 
+    signal = signal.strip()
     crash_function = ''
     
     if '_' not in func_list[0]:
@@ -119,13 +125,15 @@ def check_for_duplicates_in_db(func_list, signal):
             for row in cursor:
                 jira_ticket = row[1]
                 status = row[2]
-        except Exception as e:
-            print "unable to execute the SQL Query because %s" % str(e)
+        except MySQLdb.Error as e:
+            print "MySql Operation Failed because %s" %(str(e))
 
     if jira_ticket:
         new_status = jira_utils.search_issue(jira_ticket)
-        if new_status:
+        if new_status != status:
             status = new_status
+            update_database(jira_ticket, status)
+
 
     conn.commit()
     conn.close()
@@ -138,14 +146,36 @@ def check_for_duplicates_in_db(func_list, signal):
 def insert_into_database(issue_key, issue_status, task_id, total, title, last_line, created_date,issue_signal,function_list, **kwargs):
 
     conn = mysql_connect()
+    stack_trace = kwargs.get("stack_trace", "")
+    issue_signal = issue_signal.strip()
 
     if conn:
-        stack_trace = kwargs.get("stack_trace", "")
         cursor = conn.cursor()
-        sql = 'INSERT INTO crash_issue(issue_key, issue_status, task_id, total, title, last_line, created_date,stack_trace,issue_signal,function_list) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
-        cursor.execute(sql, (issue_key, issue_status, task_id, total, title, last_line, created_date,stack_trace,issue_signal,function_list))
-        conn.commit()
-        conn.close()
+        try:
+            sql = 'INSERT INTO crash_issue(issue_key, issue_status, task_id, total, title, last_line, created_date,stack_trace,issue_signal,function_list) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'
+            cursor.execute(sql, (issue_key, issue_status, task_id, total, title, last_line, created_date,stack_trace,issue_signal,function_list))
+        except MySQLdb.Error as e:
+            print "MySql Database Update Failed because %s hence please send details of this error to santosh@avinetworks.com" %(str(e))
+
+    conn.commit()
+    conn.close()
+
+   
+def update_database(jira_ticket, status):
+
+    conn = mysql_connect()
+
+    if conn:
+        cursor = conn.cursor()
+        try:
+            sql = "UPDATE crash_issue SET issue_status='%s' WHERE issue_key='%s'" %(status, jira_ticket)
+            cursor.execute(sql)
+        except MySQLdb.Error as e:
+            print "MySql Operation Failed because %s" %(str(e))
+            pass
+
+    conn.commit()
+    conn.close()
     
 
 
@@ -169,6 +199,10 @@ def create_jira_ticket(crash_function, contents, version, build,function_list,si
     insert_into_database(jira_ticket, status, 0, 1, summary, 'NULL', datetime.now(),signal,function_list, stack_trace=contents)
 
     return (jira_ticket, status)
+
+
+def add_jira_comment(jira_ticket, contents):
+    jira_utils.add_jira_comments(jira_ticket, contents)
     
 
 def display_results_in_tabular_format(core_file, crash_function, jira_ticket, status):
